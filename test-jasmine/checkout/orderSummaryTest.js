7dd1ac7d458b0deff17d45ca174dcd6b629afb0e
suyash9140 <suyashsingh9140@email.com>
2024-09-07 15:33:43 +0530

    Load products in beforeAll hook

--- test-jasmine/checkout/orderSummaryTest.js
+++ test-jasmine/checkout/orderSummaryTest.js
@@ -1,4 +1,5 @@
 import {  loadFromStorage,cart } from "../../data/cart.js";
+import { loadProducts } from "../../data/products.js";
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 
 
@@ -6,6 +7,14 @@ describe('test suite: render order summary', ()=>{
 
   const productId1 ='e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
   const productId2='15b6fc6f-327a-4ec4-896f-486349e85a3d';
+
+  beforeAll((done)=>{
+    loadProducts(()=>{
+      done();
+    });
+    
+  });
+
   beforeEach(()=>{
     spyOn(localStorage,'setItem');
 
@@ -26,6 +35,8 @@ describe('test suite: render order summary', ()=>{
   });
   loadFromStorage();
   renderOrderSummary();
+ 
+  
   });
 
   afterEach(() =>{
