59cffdf9197982be92b25bd3bff30b21dae01fe4
suyash9140 <suyashsingh9140@email.com>
2024-09-07 16:05:09 +0530

    Convert loadProducts from xhr to fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,16 +1,13 @@
 import { loadCart } from "../data/cart.js";
-import { loadProducts } from "../data/products.js";
+import {  loadProductsFetch } from "../data/products.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import {renderPaymentSummary} from "./checkout/paymentSummary.js";
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js'
 
 Promise.all([
-  new Promise((resolve)=>{
-    loadProducts(()=>{
-      resolve();
-    });
- }),
+ 
+ loadProductsFetch(),    
  new Promise((resolve)=>{
   loadCart(()=>{
   resolve();
--- test-jasmine/checkout/orderSummaryTest.js
+++ test-jasmine/checkout/orderSummaryTest.js
@@ -1,5 +1,5 @@
 import {  loadFromStorage,cart } from "../../data/cart.js";
-import { loadProducts } from "../../data/products.js";
+import { loadProductsFetch } from "../../data/products.js";
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 
 
@@ -9,10 +9,9 @@ describe('test suite: render order summary', ()=>{
   const productId2='15b6fc6f-327a-4ec4-896f-486349e85a3d';
 
   beforeAll((done)=>{
-    loadProducts(()=>{
-      done();
+    loadProductsFetch().then(()=>{
+       done();
     });
-    
   });
 
   beforeEach(()=>{
